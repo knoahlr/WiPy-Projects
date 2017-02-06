2b8b82715ab709ad9046a8954aef0d2545e29a77
Noah Langat <Noah@Noahs-MacBook-Air.local>
2017-02-05 23:19:56 -0500

    Minor Changes

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from machine import RTC
 import Ultrasonic
 import gc
 
+
+#Initialize RTC clock with time obtained from NIST servers.
+#Update firmware to get automatic RTC update. More reliable
 def InitRTC(CurrentTime):
     pycom.rgbled(0x0000ff)
     DateList=re.search("[0-9]+-[0-9]+-[0-9]+",CurrentTime).group(0).split("-")
@@ -43,6 +46,10 @@ def getTime():
 print("Initializing Ultrasound")
 Ultra=Ultrasonic.Ultrasound("P12","P14")
 
+#Initializing pin to listen for high output from  PIR sensor
+InputPin=Pin(,Pin.IN)
+
+
 Start=time.ticks_ms()
 while True:
     if wlan.isconnected():
@@ -51,16 +58,28 @@ while True:
         break
     End=time.ticks_ms()
     if time.ticks_diff(Start,End) > 5000:
+        pycom.heartbeat(False)
         pycom.rgbled(0xff0000)
         print("No WiFI connection, time has not been set")
         break
 
 print("Sending Data")
 pycom.heartbeat(False)
+errorFile=open("Error.log","w")
+
+#Continously send data when on. (If time was obtained)
 while True:
-    pycom.rgbled(0x0000ff)
-    posting.DataPost((("Distance",Ultra.distance_in_cm()[0]),),WIPYhttpSAS, WiPygateway_id)
-    gc.collect()
-    time.sleep_ms(100)
-pycom.heartbeat(True)
+    pycom.heartbeat(True)
     
+    try:
+        posting.DataPost((("Distance", Ultra.distance_in_cm()[0]), ), WIPYhttpSAS, WiPygateway_id)
+    except Exception as e:
+        errorFile.write("The exception is {0}".format(str(e))) 
+    gc.collect()
+    time.sleep_ms(10)
+
+errorFile.close()   
+pycom.heartbeat(False)
+pycom.rgbled(0x0000ff)
+
+
