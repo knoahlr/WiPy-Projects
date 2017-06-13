2bfe1d27e8f3f5320e2d19ef48c22658009bc14d
knoahlr <noahlangat@cmail.carleton.ca>
2017-06-13 15:52:57 -0400

    Updates

--- Infrared.py
+++ Infrared.py
@@ -1,14 +1,18 @@
+
+from machine import Pin
 import machine
-import Pin
 import time
 import os
 import posting
-
+import pycom
 #Class to manage PIR sensor, designed around having CPU paused until rising edge on PIR pin (corresponds
 # to infrared heat found)
 
 
-class Infrared:
+class InfraredClass:
+
+    WIPYhttpSAS = "SharedAccessSignature sr=https%3a%2f%2feappiotsens.servicebus.windows.net%2fdatacollectoroutbox%2fpublishers%2f435997a2-7590-45bb-ab1b-c110c743118e%2fmessages&sig=OjljnACxkfjRixCOY7MUSkDL5hH8QzNfyhadKpN55mM%3d&se=4641384176&skn=SendAccessPolicy"
+    WiPygateway_id = "435997a2-7590-45bb-ab1b-c110c743118e"
 
     def init(self, infrared_pin):
 
@@ -16,16 +20,17 @@ class Infrared:
         self.InputPin.callback(trigger=Pin.IRQ_RISING, handler=self.run)
         self.count = 0
 
-
     def run(self):
 
         machine.idle()
-        posting.DataPost((("Distance", 1), ), WIPYhttpSAS, WiPygateway_id)
-        self.humanfound()
+        #print(InfraredClass.WIPYhttpSAS, InfraredClass.WiPygateway_id)
+        posting.DataPost((("Distance", 1), ), InfraredClass.WIPYhttpSAS, InfraredClass.WiPygateway_id)
+        self.humanFound()
         machine.idle()
 
     def humanFound(self):
 
+        self.count += 1
         pycom.heartbeat(False)
         pycom.rgbled(0xff0000)
         print("{0} human(s) found".format(self.count))
--- main.py
+++ main.py
@@ -1,23 +1,6 @@
 # main.py -- put your code here!
-from time import sleep_ms
-import pycom
 
-def change_color():
-    pycom.heartbeat(False)
-    pycom.rgbled(0xff00)
 
-if InputPin:
-    count = 0
-    while True:
-        machine.pin_deepsleep_wakeup(['P13'], machine.WAKEUP_ANY_HIGH, True)
-        change_color()
-        machine.deepsleep()
-        pycom.heartbeat(True)
-        count += 1
-        if count == 40:
-            break
-
-print("Finished")
 
 
 
